fc789a4e5cc411fbf8547fb51beef6abf7949ed3
daren-thomas <dthomas.ch@gmail.com>
2016-04-28 11:05:50 +0200

    first working version!

---
+++ mysw.bat
@@ -0,0 +1,3 @@
+python mysw.py | python -m markdown > 1-now.body.html
+cat 1-now.header.html 1-now.body.html 1-now.footer.html > 1-now.html
+start 1-now.html
--- mysw.py
+++ mysw.py
@@ -1,13 +1,28 @@
 from evernote.api.client import EvernoteClient
+from evernote.edam.notestore.ttypes import NoteFilter, NotesMetadataResultSpec
 
 dev_token = open('devtoken', 'r').read().rstrip()
-print dev_token
-client = EvernoteClient(token=dev_token)
+client = EvernoteClient(token=dev_token, sandbox=False)
 userStore = client.get_user_store()
 user = userStore.getUser()
-print user.username
 
 note_store = client.get_note_store()
+
+# find "Action-Pending" notebook
 notebooks = note_store.listNotebooks()
-for n in notebooks:
-    print n.name
+action_pending = [n for n in notebooks if n.name == "Action-Pending"][0]
+
+# collect all tags
+tags = {t.name: t for t in note_store.listTags()}
+
+for location_tag in sorted([tags[key] for key in tags.keys() if key.startswith('@')]):
+    print '#', location_tag.name
+    print
+    note_filter = NoteFilter()
+    note_filter.tagGuids = [location_tag.guid, tags['1-Now'].guid]
+    result_spec = NotesMetadataResultSpec()
+    result_spec.includeTitle = True
+    notes = note_store.findNotesMetadata(note_filter, 0, 100, result_spec)
+    for n in notes.notes:
+        print '-', n.title
+    print
