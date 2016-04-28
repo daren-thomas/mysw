9f977e09b079f06b87f69fe5987636516ece8a1a
daren-thomas <dthomas.ch@gmail.com>
2016-04-28 14:11:01 +0200

    added tags to output

--- mysw.py
+++ mysw.py
@@ -14,6 +14,7 @@ action_pending = [n for n in notebooks if n.name == "Action-Pending"][0]
 
 # collect all tags
 tags = {t.name: t for t in note_store.listTags()}
+tag_names = {t.guid: t.name for t in note_store.listTags()}
 
 for location_tag in sorted([tags[key] for key in tags.keys() if key.startswith('@')]):
     print '#', location_tag.name
@@ -22,7 +23,10 @@ for location_tag in sorted([tags[key] for key in tags.keys() if key.startswith('
     note_filter.tagGuids = [location_tag.guid, tags['1-Now'].guid]
     result_spec = NotesMetadataResultSpec()
     result_spec.includeTitle = True
+    result_spec.includeTagGuids = True
     notes = note_store.findNotesMetadata(note_filter, 0, 100, result_spec)
     for n in notes.notes:
-        print '-', n.title
+        print '-', n.title, ' '.join('*%s*' % tag_names[guid]
+                                     for guid in n.tagGuids
+                                     if not tag_names[guid] in {'1-Now', location_tag.name})
     print
