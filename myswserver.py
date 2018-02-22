0681a95ec989051307d370df227aa30e8219c9df
Daren Thomas <thomas@arch.ethz.ch>
2018-02-22 14:19:37 +0100

    following miguels mega tutorial on flask (ditching htmlpy)

--- myswserver.py
+++ myswserver.py
@@ -1,16 +1,2 @@
 """A web app for mysw"""
-from evernote.api.client import EvernoteClient
-from evernote.edam.notestore.ttypes import NoteFilter, NotesMetadataResultSpec
-
-from flask import Flask, render_template
-app = Flask(__name__)
-
-@app.route("/") # take note of this decorator syntax, it's a common pattern
-def hello():
-    user = {'nickname': 'Miguel'}  # fake user
-    return render_template('index.html',
-                           title='Home',
-                           user=user)
-
-if __name__ == "__main__":
-    app.run()
+from app import app
