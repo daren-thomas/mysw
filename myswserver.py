16b1d388f83084fa0ce08ee1378286a3f193d1ae
Daren Thomas <thomas@arch.ethz.ch>
2018-07-11 12:52:13 +0200

    deleting some stuff i think is not used anymore

--- myswserver.py
+++
@@ -1,8 +0,0 @@
-"""A web app for mysw"""
-from app import app
-from app.models import db, User, Post
-
-
-@app.shell_context_processor
-def make_shell_context():
-    return {'db': db, 'User': User, 'Post': Post}
